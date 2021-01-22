7123b297562de72f2362d82e7b3ee6e951fff5f7
akshithvarma <akshithchittiveli@gmail.com>
2021-01-22 16:07:00 +0530

    --gateway -stable

---
+++ gateway/src/test/java/com/quinbay/commoninfra/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quinbay.commoninfra.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ registry/src/test/java/com/quinbay/commoninfra/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quinbay.commoninfra.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
