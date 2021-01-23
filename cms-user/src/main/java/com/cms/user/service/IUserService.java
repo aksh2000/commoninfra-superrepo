528d750d85431b8b49a83612fa399537423b75bf
akshayk17 <akshay.k17@iiits.in>
2021-01-23 15:00:05 +0530

    --cms --added user jpa functions

--- cms-user/src/main/java/com/cms/user/service/IUserService.java
+++ cms-user/src/main/java/com/cms/user/service/IUserService.java
@@ -1,4 +1,11 @@
 package com.cms.user.service;
 
+import com.cms.user.entity.User;
+
+import java.util.Optional;
+
 public interface IUserService {
+    Optional<User> findById(Long userId);
+
+    User addUser(User user);
 }
