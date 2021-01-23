63a4956e8e212f965be52941c24e7178127ac637
daxesh020500 <43899470+daxesh020500@users.noreply.github.com>
2021-01-23 14:59:12 +0530

    --cms --quiz type changed to integer

--- cms-admin/src/main/java/com/cms/admin/repository/AdminRepository.java
+++ cms-admin/src/main/java/com/cms/admin/repository/AdminRepository.java
@@ -1,8 +1,10 @@
 package com.cms.admin.repository;
 
+import com.cms.admin.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdminRepository  {
+public interface AdminRepository extends JpaRepository<Admin,Long> {
 }
--- cms-admin/src/main/java/com/cms/admin/service/IAdminService.java
+++ cms-admin/src/main/java/com/cms/admin/service/IAdminService.java
@@ -1,4 +1,11 @@
 package com.cms.admin.service;
 
+import com.cms.admin.entity.Admin;
+
+import java.util.Optional;
+
 public interface IAdminService {
+    Optional<Admin> findById(Long adminId);
+
+    Admin addAdmin(Admin admin);
 }
