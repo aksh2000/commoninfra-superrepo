f19f1126c4b472c3a7a1c49ef632869059b25dec
akshayk17 <akshay.k17@iiits.in>
2021-01-23 14:24:42 +0530

    Merge branch 'quiz' of https://github.com/aksh2000/commoninfra-superrepo into quiz
    # Conflicts:
    #	.idea/workspace.xml

--- cms-admin/src/main/java/com/cms/admin/controller/AdminController.java
+++ cms-admin/src/main/java/com/cms/admin/controller/AdminController.java
@@ -6,4 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/cmsAdmin")
 public class AdminController {
+
 }
--- cms-user/src/main/java/com/cms/user/entity/User.java
+++ cms-user/src/main/java/com/cms/user/entity/User.java
@@ -1,4 +1,25 @@
 package com.cms.user.entity;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    String userId;
+    String firstName;
+    String lastName;
+    String email;
+    String phone;
+
+
 }
+
+
+
