1772a5b23a69d3249e2d72db92b4c7afa16c55ea
daxesh020500 <43899470+daxesh020500@users.noreply.github.com>
2021-01-24 18:54:56 +0530

    --cms  added NonScreenedQuestions

---
+++ cms-admin/src/main/java/com/cms/admin/entity/NonScreenedQuestions.java
@@ -0,0 +1,26 @@
+package com.cms.admin.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "nonScreenedQuestions")
+@Data
+public class NonScreenedQuestions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long questionId;
+    private String content;
+    private String url;
+    private int level;
+    private int type;
+    private Long categoryId;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String rightAnswer;
+    private int noOfCorrectAttempt;
+    private int maxTime;
+}
